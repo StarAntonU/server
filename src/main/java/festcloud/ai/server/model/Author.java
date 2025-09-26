9c4b32780c37b19d0be12ad404f3936109762851
Anton <StarAntonU@Gmail.com>
2025-09-26 16:38:31 +0300

    create scheme liquibase and run app

--- src/main/java/festcloud/ai/server/model/Author.java
+++ src/main/java/festcloud/ai/server/model/Author.java
@@ -1,10 +1,10 @@
 package festcloud.ai.server.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.util.Set;
@@ -19,6 +19,7 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
